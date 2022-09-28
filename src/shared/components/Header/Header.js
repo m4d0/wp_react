e15fc11accd03597593b4aeda4770f332336840c
Marco Donati <marco.donati74@gmail.com>
2022-09-28 07:38:49 +0200

    start...

--- src/shared/components/Header/Header.js
+++ src/shared/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styles from './Header.module.css'
 
-const Header = () => <div className={styles.Header}></div>
+const Header = () => <div className={styles.Header}>puppa</div>
 
 Header.propTypes = {}
 
